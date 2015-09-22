680c2d408a37d33427019a227e9d561c9ff539b6
Quang béo <quangbeo216@gmail.com>
2015-09-22 14:51:35 +0700

    input

---
+++ app/src/main/java/com/p4t2xtynaexample/myapplication/About.java
@@ -0,0 +1,15 @@
+package com.p4t2xtynaexample.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by _p4t2xty_NA_ on 4/19/2015.
+ */
+public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
